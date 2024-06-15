7d8842115f4117cbc21e7e45e07fd5a227e238c7
Rdrudra99 <rd.rudra99@gmail.com>
2024-06-15 15:29:35 +0530

    rudra

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const simpleGit = require("simple-git");
 const random = require("random");
 const FILE_PATH = "./data.json";
 const makeCommit = (n) => {
+  if (n === 0) return simpleGit().push();
   const x = random.int(0, 54);
   const y = random.int(0, 6);
   const DATE = moment()
@@ -15,10 +16,12 @@ const makeCommit = (n) => {
   const data = {
     date: DATE,
   };
+  console.log(DATE);
+  jsonfile.writeFile(FILE_PATH, () => {
+    simpleGit()
+      .add([FILE_PATH])
+      .commit(DATE, { "--date": DATE }, makeCommit.bind(this, --n));
+  });
 };
-
-jsonfile.writeFile(FILE_PATH, () => {
-  simpleGit().add([FILE_PATH]).commit(DATE, { "--date": DATE }).push();
-});
 
 makeCommit(100);
